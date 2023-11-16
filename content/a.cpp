db46c765b5af845e7c4c52e3dec691bae424242b
mengxain0913 <mengxian0913@gmail.com>
2023-11-16 21:45:27 +0800

    first commit

---
+++ content/a.cpp
@@ -0,0 +1,31 @@
+/*********************************************************
+# File Name: a.cpp
+# Author: Vincent
+# Mail: mengxian0913@gmail.com
+# Created Time: 四 11/16 21:06:37 2023
+*********************************************************/
+
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ff first
+#define ss second
+#define fastIO cin.tie(nullptr)->sync_with_stdio(false);
+#define INF 0x7FFFFFFF
+#define pb push_back
+#define all(aa) aa.begin(),aa.end()
+#define MOD (int)(1e9+7)
+
+void solve(){
+  int n;
+  cin >> n;
+  cout << n << "\n";
+  return;
+}
+
+signed main(){
+  fastIO
+  solve();
+  return 0;
+}
